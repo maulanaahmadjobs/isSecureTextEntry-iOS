db34e0a1957d36b5347defb6be99f1bd384937c0
Maulana West <maulanaahmadjobs@gmail.com>
2025-10-28 10:37:37 +0700

    Initial Commit

---
+++ isSecureTextEntry iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  isSecureTextEntry iOS
+//
+//  Created by west on 28/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ isSecureTextEntry iOSUITests/isSecureTextEntry_iOSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  isSecureTextEntry_iOSUITestsLaunchTests.swift
+//  isSecureTextEntry iOSUITests
+//
+//  Created by west on 28/10/25.
+//
+
+import XCTest
+
+final class isSecureTextEntry_iOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
